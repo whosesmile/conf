a110ee93804eee10f0235b9fc8a46c4f1cd9e646
shuangbao.lsb <shuangbao.lsb@alibaba-inc.com>
2014-09-09 09:53:46 +0800

    fix js

--- src/js/app/home.js
+++ src/js/app/home.js
@@ -1,7 +1,21 @@
 $(function () {
-  $('.list-actions li.settings').click(function () {
+  $('.list-actions li.settings').on('click', function (e) {
     if (!$(this).is('.disabled')) {
       $(this).toggleClass('active');
     }
+    e.stopPropagation();
+  });
+
+  $('.list-actions li.settings .options').on('click', function (e) {
+    e.stopPropagation();
+  });
+
+  $('.list-actions li.settings .options .btn').click(function (e) {
+    $(this).closest('li').removeClass('active');
+    e.stopPropagation();
+  });
+
+  $(document).on('click', function () {
+    $('.list-actions li.settings').removeClass('active');
   });
 });
