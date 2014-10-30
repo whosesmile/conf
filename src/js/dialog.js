864ec3656ad0e9b89de48ea1cd7220dd70a02a36
shuangbao.lsb <shuangbao.lsb@alibaba-inc.com>
2014-10-30 17:07:06 +0800

    delete unuse files

--- src/js/dialog.js
+++
@@ -1,9 +0,0 @@
-$(document).on('click', '#content .wrapper button', function () {
-  var template = $('#' + $(this).attr('template')).html();
-  console.log($(this).attr('template'))
-  new CommonDialog({
-    noheader: true,
-    width: 460,
-    message: Mustache.render($.trim(template), {})
-  });
-});
